f714318d1098d6cdca722e3c3f1130aad1ed00f2
xiaojibadada <84449267+xiaojibadada@users.noreply.github.com>
2021-09-12 08:59:21 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,7 +16,7 @@
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 git clone https://github.com/CHN-beta/rkp-ipid package/rkp-ipid
-git clone https://github.com/SunBK201/UA2F.git package/UA2F
+git clone https://github.com/DFlinmo/UA2F package/UA2F
 cd package/UA2F
-git checkout bugfix
+git checkout 20210531T234622
 cd ../..
